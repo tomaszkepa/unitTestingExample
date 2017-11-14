a452a17d09207893458324ee0dcb1d775d94b914
Tomasz Kepa <tomasz.kepa@grandparade.co.uk>
2017-11-14 16:02:39 +0100

    fix: Update render.test.js

--- test/render.test.js
+++ test/render.test.js
@@ -1,10 +1,12 @@
 const test = require("tape");
 const h = require("../src/h");
-const render = require("../src/render");
+const renderer = require("../src/render");
 require("jsdom-global")();
 
 test("complex example", function (t) {
     document.body.innerHTML = "";
+    const render = renderer();
+
     render(
         h("div", {class: "outer"}, [h("div", {class: "inner"}, ["text"])])
     );
